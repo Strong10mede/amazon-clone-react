592279f4fab55800c73474b1a500cbdae6b40fa7
Strong10mede <kmayur809@gmail.com>
2023-01-04 13:59:16 +0530

    x

--- src/firebase.js
+++ src/firebase.js
@@ -1,17 +1,15 @@
 import firebase from "firebase";
 
 const firebaseApp = firebase.initializeApp({
-    apiKey: "AIzaSyA_Tyd3SyJ_5RA4DoiIlD32f4LgpmqOvrU",
-    authDomain: "clone-31d90.firebaseapp.com",
-    databaseURL: "https://clone-31d90.firebaseio.com",
-    projectId: "clone-31d90",
-    storageBucket: "clone-31d90.appspot.com",
-    messagingSenderId: "1061472110930",
-    appId: "1:1061472110930:web:f73b83fcb2adc4e9b9d28b",
-    measurementId: "G-LQK4QBJH79"
+  apiKey: "AIzaSyB9si1CZ2qkA9YvsRY_UKW3zSqfVyK4ebQ",
+  authDomain: "clone-react-1f23c.firebaseapp.com",
+  projectId: "clone-react-1f23c",
+  storageBucket: "clone-react-1f23c.appspot.com",
+  messagingSenderId: "760814295302",
+  appId: "1:760814295302:web:e99c0a75528628fd252687",
 });
 
 const db = firebaseApp.firestore();
 const auth = firebase.auth();
 
-export { db,auth };
+export { db, auth };
